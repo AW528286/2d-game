40c490526573e3e3afe61e750186e5bb5a60dff3
AW528286 <113694026+AW528286@users.noreply.github.com>
2022-11-01 16:55:20 +0000

    men 2d game test

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -8,12 +8,14 @@ public class Movement : MonoBehaviour
     public float speed = 5f;
 
     Rigidbody2D rb;
+    SpriteRenderer sprite;
 
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -23,5 +25,13 @@ public class Movement : MonoBehaviour
         float dirY = Input.GetAxisRaw("Vertical"); 
 
         rb.velocity = new Vector2 (dirX * speed, dirY * speed);
+        if (dirX < 0)
+        {
+            sprite.flipX = true;
+        }
+        else
+        {
+            sprite.flipX = false;
+        }
     }
 }
